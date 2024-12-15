7b5465608e6c36c99c3e6dd20213eec0502e0474
Vinícius Ferreira de Oliveira <vinicius.ferreira22615@gmail.com>
2024-12-15 00:49:08 -0300

    feat: stream conversation added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const $prompt  = document.getElementById('prompt-input');
 const $chatbox = document.getElementById('chatbox');
+let id = 0;
 
 $prompt.addEventListener('keypress', async (e) => {
     if (e.key === 'Enter') {
@@ -14,22 +15,32 @@ function createPromptCard() {
     const promptCard = document.createElement('div');
     promptCard.className = 'prompt-card';
     const span = document.createElement('span');
-    span.innerHTML = $prompt.value;
     promptCard.appendChild(span);
     $chatbox.appendChild(promptCard);
     $prompt.value = '';
+    
 }
 
-function createResponseCard(response) {
+function createResponseCard() {
     const responseCard = document.createElement('div');
     responseCard.className = 'response-card';
+    responseCard.id = id + 1;
+    id ++;
     const span = document.createElement('span');
-    span.innerHTML = response;
     responseCard.appendChild(span);
     $chatbox.appendChild(responseCard);
+    return responseCard.id;
+}
+
+function updateResponseCard(id, response) {
+    const responseCard = document.getElementById(id);
+    const span = responseCard.children[0];
+    span.innerHTML += response;
 }
 
 async function sendPrompt(content) {
+    const responseCardId = createResponseCard();
+
     const url = 'http://localhost:11434/v1/chat/completions';
     const headers = {
         'Content-Type': 'application/json',
@@ -42,7 +53,7 @@ async function sendPrompt(content) {
                 content,
             }
         ],
-        stream: false
+        stream: true
     });
 
     try {
@@ -54,8 +65,21 @@ async function sendPrompt(content) {
 
         if (!response.ok) throw new Error(JSON.stringify(response));
 
-        const data = await response.json();
-        createResponseCard(data.choices[0].message.content);
+        const reader = response.body.getReader();
+        const decoder = new TextDecoder();
+        let chunk = '';
+
+        reader.read().then(function processStream({ done, value }) {
+            if (done) {
+                return;
+            }
+
+            chunk = JSON.parse(decoder.decode(value, {stream: true}).split("data: ")[1]);
+            const content = chunk.choices[0].delta.content;
+            updateResponseCard(responseCardId, content);
+            reader.read().then(processStream);
+        });
+
     } catch (error) {
         createResponseCard(error.message);
     }
